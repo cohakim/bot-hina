18fd207c1f344855896f8d28b18dc2b352018186
cohakim <cohakim@gmail.com>
2015-12-19 17:47:33 +0900

    Rename bot name

--- lita_config.rb
+++ lita_config.rb
@@ -1,5 +1,5 @@
 Lita.configure do |config|
-  config.robot.name = "Lita"
+  config.robot.name = "hina"
   config.robot.log_level = :info
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_API_TOKEN']
